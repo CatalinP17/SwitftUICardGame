912abe978b22b5aedeabd21aced17f0046e40695
Catalin Petre <petrecatalin42@gmail.com>
2022-06-29 16:31:03 +0300

    Initial commit

---
+++ SwiftUICardGame/SwiftUICardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICardGameApp.swift
+//  SwiftUICardGame
+//
+//  Created by Catalin Petre on 16/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
